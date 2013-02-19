aaffaa57a0ba848e535bcdddc378b877c68394b2
Takuya Ueda <uedatakuya@gmail.com>
2013-02-20 06:06:55 +0900

    add two samples

---
+++ mux/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello")
+}
+
+func main() {
+
+	handlers := http.NewServeMux()
+	handlers.HandleFunc("/1", handler)
+	server := &http.Server{Addr: ":8080", Handler: handlers}
+
+	go server.ListenAndServe()
+
+	handlers.HandleFunc("/2", handler)
+
+	select {}
+
+}
